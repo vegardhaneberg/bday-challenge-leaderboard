7ce459eb50bfab3690f780a646b89aa0a711f495
Vegard Haneberg <vegard.haneberg@live.no>
2024-02-14 19:39:42 +0100

    Read data from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,62 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { db } from "./firebaseConfig";
+import { get, ref } from "firebase/database";
 
 export interface LeaderboardItem {
   name: string;
-  time: string;
+  time: number;
 }
 
-const leaderboardData: LeaderboardItem[] = [
-  { name: "Larsi", time: "15" },
-  { name: "Syver", time: "15.5" },
-  { name: "Martin", time: "16" },
-  { name: "Sverre", time: "17" },
-  { name: "Mads", time: "18.3" },
-  { name: "Agnes", time: "18.8" },
-  { name: "Elise", time: "20.7" },
-  { name: "Alfred", time: "21" },
-  { name: "Vegard", time: "22" },
-  { name: "Sigurd", time: "23.18" },
-  { name: "Håkon", time: "24.65" },
-  { name: "Eivind", time: "27.17" },
-  { name: "Katty", time: "29.00" },
-  { name: "Thomas", time: "29.96" },
-  { name: "Bea", time: "32.00" },
-  { name: "Daniel", time: "37.00" },
-  { name: "Kate", time: "45.00" },
-];
-
 function App() {
+  const [data, setData] = useState<LeaderboardItem[]>();
+
+  useEffect(() => {
+    const dataRef = ref(db, "items");
+    get(dataRef).then((firebaseData) => {
+      const convertedData: LeaderboardItem[] = Object.values(
+        firebaseData.val()
+      );
+      setData(convertedData);
+    });
+  }, []);
+
   return (
     <div className="leaderboard">
-      <h1>Bday Challenge leaderboard</h1>
+      <h1>Bday Challenge</h1>
       <ul>
-        {leaderboardData.map((item, i) => (
-          <li key={i} className="leaderboard-item">
-            <span className="rank">{i + 1}</span>
-            <span className="name">{item.name}</span>
-            <span className="time">{item.time}</span>
-          </li>
-        ))}
+        {data &&
+          data.map((item, i) => (
+            <li key={i} className="leaderboard-item">
+              <span className="rank">{i + 1}</span>
+              <span className="name">{item.name}</span>
+              <span className="time">{item.time.toFixed(2)}</span>
+            </li>
+          ))}
       </ul>
     </div>
+    // <div className="leaderboard">
+    //   <h1>Leaderboard</h1>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>Rank</th>
+    //         <th>Name</th>
+    //         <th>Time</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {data &&
+    //         data.map((item, index) => (
+    //           <tr key={index}>
+    //             <td>{index + 1}</td>
+    //             <td>{item.name}</td>
+    //             <td>{item.time.toFixed(2)}</td>
+    //           </tr>
+    //         ))}
+    //     </tbody>
+    //   </table>
+    // </div>
   );
 }
 
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQQmi6tSSjFpGwRk_OI-V2_EBzEMnl-ms",
+  authDomain: "bdaychallenge-da960.firebaseapp.com",
+  databaseURL:
+    "https://bdaychallenge-da960-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bdaychallenge-da960",
+  storageBucket: "bdaychallenge-da960.appspot.com",
+  messagingSenderId: "1005972264441",
+  appId: "1:1005972264441:web:642a9586fb4804802e9928",
+  measurementId: "G-815BW30PNV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
